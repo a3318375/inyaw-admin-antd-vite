a6f055f573dac9a2a4e1ddb3ff7e6ca7e124bb0a
a3318375 <184608371@qq.com>
2024-03-22 15:24:52 +0800

    gh-page

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 import { viteMockServe } from "vite-plugin-mock";
 import generouted from '@generouted/react-router/plugin'
 
-
-const isDev = process.env.NODE_ENV === 'development';
-// https://vitejs.dev/config/
 export default defineConfig({
     resolve: {
         alias: {
